8f1c01ebc9b430b6daac0d760a60749baca44765
Augusto Vivaldelli <augusto.a.vivaldelli@gmail.com>
2026-01-02 13:31:13 -0300

    FIX: fix typo

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ export * from "./types/effect";
 export * from "./types/asyncEffect";
 export * from "./stream/stream";
 export * from "./types/option";
-export * from "./types/effect";
 
+export * from "./runtime/runtime";
+export * from "./runtime/fiber";
 export * from "./runtime/scope";
 export * from "./runtime/scheduler";
 export * from "./types/cancel";
