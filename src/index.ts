fc09686e8bd77352063899be4886cd416ef93f74
Augusto Vivaldelli <augusto.a.vivaldelli@gmail.com>
2026-01-02 13:32:27 -0300

    FIX: fix index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 export * from "./types/effect";
 export * from "./types/asyncEffect";
-export * from "./stream/stream";
 export * from "./types/option";
+export * from "./types/cancel";
+export * from "./stream/stream";
 
 export * from "./runtime/runtime";
 export * from "./runtime/fiber";
 export * from "./runtime/scope";
 export * from "./runtime/scheduler";
-export * from "./types/cancel";
+
 
